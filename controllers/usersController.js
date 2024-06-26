114859bd26f557b2e51a6ff479b40d6312c5da25
Mohit Sengar <mohitsingh.shiv@gmail.com>
2024-06-26 19:34:36 +0000

    add id field

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,7 +4,7 @@ const getUserDetails = async (req, res) => {
     const userId = req.params.id;
 
     try {
-        const user = await User.findOne({ id: userId }).select('-_id -__v -id');
+        const user = await User.findOne({ id: userId }).select('-_id -__v');
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
