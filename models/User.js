abd9ff2287ab13ab6467404f4b0153d9a410d30f
Mohit Sengar <mohitsingh.shiv@gmail.com>
2024-06-27 01:20:47 +0000

    update

--- models/User.js
+++ models/User.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const { DateTime } = require('luxon');
 
+// Function to get the ordinal suffix of a number
+function getOrdinalSuffix(day) {
+    const j = day % 10,
+        k = day % 100;
+    if (j == 1 && k != 11) {
+        return 'st';
+    }
+    if (j == 2 && k != 12) {
+        return 'nd';
+    }
+    if (j == 3 && k != 13) {
+        return 'rd';
+    }
+    return 'th';
+}
+
 const userSchema = new mongoose.Schema({
     id: { 
         type: Number, 
@@ -25,7 +41,11 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.toJSON = function() {
     const obj = this.toObject();
     if (obj.birthday) {
-        obj.birthday = DateTime.fromJSDate(obj.birthday).toFormat('MMMM dd, yyyy');
+        let dt = DateTime.fromJSDate(obj.birthday);
+        let day = dt.toFormat('d');
+        let month = dt.toFormat('LLLL');
+        let year = dt.toFormat('yyyy');
+        obj.birthday = `${month} ${day}${getOrdinalSuffix(day)}, ${year}`;
     }
     return obj;
 };
